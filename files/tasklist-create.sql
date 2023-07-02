6bbd2a6c63d0747c7724feed5ad3208ad3df2cbd
Trevor Curtis <trevorscurtis@gmail.com>
2023-07-02 15:55:23 -0400

    Adding PUBLIC permission.

--- files/tasklist-create.sql
+++ files/tasklist-create.sql
@@ -1,3 +1,4 @@
 create database tasklistdb;
 create user tasklist with encrypted password 'tasklist';
 grant all privileges on database tasklistdb to tasklist;
+alter database tasklistdb owner to tasklist;
